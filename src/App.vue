07ddab4e0902e99b4a089714497a98e660662d9f
markwst <Wongsathorn_W@gmail.com>
2023-06-16 20:41:27 +0700

    change navbar position and alignment

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/curriculum">Curriculum</router-link> |
-    <router-link to="/course_list">Course list</router-link>
-  </nav>
-  <router-view />
+  <div class="container">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/curriculum">Curriculum</router-link>
+      <router-link to="/course_list">Course list</router-link>
+    </nav>
+<div class="content">
+
+
+    <router-view />
+  </div></div>
 </template>
 
 <style>
@@ -15,18 +20,33 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
 nav {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   padding: 30px;
+  background-color: #010508;
 }
 
 nav a {
+  margin-bottom: 10px;
   font-weight: bold;
   color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+.container{
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  height: 100vh;
+}
+.content {
+  padding: 30px;
+
 }
 </style>
