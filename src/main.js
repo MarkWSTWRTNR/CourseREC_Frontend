2f4e257cc1c811967d5ae962d25de5a8293c3559
markwst <Wongsathorn_W@gmail.com>
2023-07-01 23:31:26 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 import store from "./store";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
-createApp(App).use(store).use(router).mount("#app");
+import vSelect from "vue-select"
+import "vue-select/dist/vue-select.css"
+const app = createApp(App);
+app.component("v-select",vSelect)
+app.use(store)
+app.use(router)
+app.mount("#app");
