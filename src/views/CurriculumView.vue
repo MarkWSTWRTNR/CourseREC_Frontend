1e17987f974dbcc6a4b1d48298dc6a1352414c4f
markwst <Wongsathorn_W@gmail.com>
2023-07-02 15:26:26 +0700

    can select exising the programs matching existing the faculty

--- src/views/CurriculumView.vue
+++ src/views/CurriculumView.vue
@@ -1,104 +1,80 @@
 <template>
   <div>
-    <router-link to="/faculty"><button> Create Faculty</button></router-link>
-    <router-link to="/courseprogram"><button> Create Program</button></router-link>
-  </div>
-  <div>
-    <select v-model="selectedFaculty" @change="updatePrograms">
-      <option value="">Select Faculty</option>
-      <option v-for="faculty in faculties" :value="faculty.id">{{ faculty.name }}</option>
-    </select>
-    <select v-model="selectedProgram">
-      <option value="">Select Program</option>
-      <option v-for="program in filteredPrograms" :value="program.id">{{ program.name }}</option>
-    </select>
-  </div>
-  <div>
-    <div v-for="(item, index) in items" :key="index">
-      <div @click="toggleAccordion(index)" :class="{ 'active': isActive(index) }">
-        <h3>{{ item.title }}</h3>
-        <i class="fa fa-chevron-down" :class="{ 'fa-rotate-180': isActive(index) }"></i>
+    <div>
+      <router-link to="/faculty"><button> Create Faculty</button></router-link>
+      <router-link to="/courseprogram"><button> Create Program</button></router-link>
+    </div>
+    <div>
+      <h3>Select Faculty:</h3>
+      <select v-model="selectedFaculty">
+        <option value="">-- Select Faculty --</option>
+        <option v-for="faculty in faculties" :value="faculty.faculty_id" :key="faculty.faculty_id">
+          {{ faculty.faculty_name }}
+        </option>
+      </select>
+
+      <h3 v-if="selectedFaculty">Select Program:</h3>
+      <select v-if="selectedFaculty" v-model="selectedProgram">
+        <option value="">-- Select Program --</option>
+        <option v-for="program in programs" :value="program.program_id" :key="program.program_id">
+          {{ program.program_name }}
+        </option>
+      </select>
+
+      <div v-if="selectedFaculty">
+        <h4>Selected Faculty:</h4>
+        <p>Faculty ID: {{ selectedFaculty }}</p>
       </div>
-      <div v-show="isActive(index)" class="content">
-        <!-- Content for each accordion item -->
-        <p>{{ getProgramContent(item.content) }}</p>
+
+      <div v-if="selectedProgram">
+        <h4>Selected Program:</h4>
+        <p>Program ID: {{ selectedProgram }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      items: [
-        { title: 'Curriculum structure', content: 'Content for Accordion Item 1' },
-        { title: 'Study plan', content: 'Content for Accordion Item 2' },
-      ],
-      activeIndices: [],
-      faculties: [
-        { id: 1, name: 'Faculty 1' },
-        { id: 2, name: 'Faculty 2' },
-        { id: 3, name: 'Faculty 3' },
-      ],
-      programs: [
-        { id: 1, name: 'Program 1', facultyId: 1 },
-        { id: 2, name: 'Program 2', facultyId: 1 },
-        { id: 3, name: 'Program 3', facultyId: 2 },
-        { id: 4, name: 'Program 4', facultyId: 3 },
-      ],
+      faculties: [],
+      programs: [],
       selectedFaculty: '',
       selectedProgram: '',
-      acticeIndices:[]
     };
   },
   methods: {
-    toggleAccordion(index) {
-      if (this.isActive(index)) {
-        this.activeIndices = this.activeIndices.filter(i => i !== index);
-      } else {
-        this.activeIndices.push(index);
-      }
-    },
-    isActive(index) {
-      return this.activeIndices.includes(index);
-    },
-    onFacultyChange() {
-      this.selectedProgram = ''; // Reset selected program when faculty changes
-    },
-    onProgramChange() {
-      this.activeIndices = []; // Reset active indices when program changes
-    },
-    getProgramContent(content) {
-      // Logic to retrieve program-specific content based on selected faculty and program
-      // You can modify this logic based on your specific requirements
-      if (this.selectedFaculty && this.selectedProgram) {
-        // Retrieve the program-specific content based on selected faculty and program
-        // For example, you can use an object or API call to fetch the content
-        // Replace the placeholder content with the actual program-specific content
-        return `Content for ${this.selectedFaculty}, ${this.selectedProgram}: ${content}`;
-      }
-      return content;
-    }
+    async fetchFacultiesAndPrograms() {
+      try {
+        const facultiesResponse = await axios.get('http://localhost:5000/api/faculties');
+        this.faculties = facultiesResponse.data;
+        console.log(this.faculties)
 
-  },
-  computed: {
-    filteredPrograms() {
-      if (this.selectedFaculty) {
-        return this.programs.filter(program => program.facultyId === this.selectedFaculty);
+        if (this.selectedFaculty) {
+          const programsResponse = await axios.get('http://localhost:5000/api/programs', {
+            params: {
+              faculty_id: this.selectedFaculty,
+            },
+          });
+          this.programs = programsResponse.data;
+          console.log(this.programs)
+        }
+      } catch (error) {
+        console.error(error);
       }
-      return [];
     },
+  },
+  watch: {
+    selectedFaculty() {
+      this.fetchFacultiesAndPrograms();
+    },
+  },
+  mounted() {
+    this.fetchFacultiesAndPrograms();
   },
 };
 </script>
 
-<style>
-.active {
-  background-color: #f0f0f0;
-}
-
-.content {
-  padding: 10px;
-}
-</style>
