16c1fba2d8d926f1411166859ef4ba5b2d9a9f2b
markwst <Wongsathorn_W@gmail.com>
2023-07-02 17:10:11 +0700

    remove sub accordion

--- src/views/CurriculumView.vue
+++ src/views/CurriculumView.vue
@@ -21,31 +21,15 @@
         </option>
       </select>
 
-
       <div v-if="selectedProgram">
-        <div>
-          <div v-for="(item, index) in items" :key="index">
-            <div @click="toggleAccordion(index)" :class="{ 'active': isActive(index) }">
-              <h3>{{ item.title }}</h3>
-              <i class="fa fa-chevron-down" :class="{ 'fa-rotate-180': isActive(index) }"></i>
-            </div>
-            <div v-show="isActive(index)" class="content">
-              <!-- Content for each accordion item -->
-              <p>{{ item.content }}</p>
-
-              <div>
-                <div v-for="(subItem, subIndex) in item.subItems" :key="subIndex">
-                  <div @click="toggleSubAccordion(index, subIndex)" :class="{ 'active': isSubActive(index, subIndex) }">
-                    <h4>{{ subItem.title }}</h4>
-                    <i class="fa fa-chevron-down" :class="{ 'fa-rotate-180': isSubActive(index, subIndex) }"></i>
-                  </div>
-                  <div v-show="isSubActive(index, subIndex)" class="sub-content">
-                    <p>{{ subItem.content }}</p>
-                  </div>
-                </div>
-              </div>
-
-            </div>
+        <div v-for="(item, index) in items" :key="index">
+          <div @click="toggleAccordion(index)" :class="{ 'active': isActive(index) }">
+            <h3>{{ item.title }}</h3>
+            <i class="fa fa-chevron-down" :class="{ 'fa-rotate-180': isActive(index) }"></i>
+          </div>
+          <div v-show="isActive(index)" class="content">
+            <!-- Content for each accordion item -->
+            <p>{{ item.content }}</p>
           </div>
         </div>
       </div>
@@ -63,26 +47,17 @@ export default {
         {
           title: 'Curriculum structure',
           content: 'Content for Accordion Item 1',
-          subItems: [
-            { title: 'Sub Item 1', content: 'Content for Sub Item 1' },
-            { title: 'Sub Item 2', content: 'Content for Sub Item 2' }
-          ]
         },
         {
           title: 'Study plan',
           content: 'Content for Accordion Item 2',
-          subItems: [
-            { title: 'Sub Item 3', content: 'Content for Sub Item 3' },
-            { title: 'Sub Item 4', content: 'Content for Sub Item 4' }
-          ]
-        }
+        },
       ],
       faculties: [],
       programs: [],
       selectedFaculty: '',
       selectedProgram: '',
       activeIndices: [],
-      subActiveIndices: [], // Add this line
     };
   },
   methods: {
@@ -114,19 +89,6 @@ export default {
     },
     isActive(index) {
       return this.activeIndices.includes(index);
-    },
-    toggleSubAccordion(index, subIndex) {
-      if (this.isSubActive(index, subIndex)) {
-        this.subActiveIndices[index] = this.subActiveIndices[index].filter(i => i !== subIndex);
-      } else {
-        if (!this.subActiveIndices[index]) {
-          this.subActiveIndices[index] = [];
-        }
-        this.subActiveIndices[index].push(subIndex);
-      }
-    },
-    isSubActive(index, subIndex) {
-      return this.subActiveIndices[index] && this.subActiveIndices[index].includes(subIndex);
     },
   },
   watch: {
