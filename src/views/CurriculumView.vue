55ba7e4759e6112a91b4b94cda26e9c58b564a26
markwst <Wongsathorn_W@gmail.com>
2023-07-21 01:30:58 +0700

    update curriculum selecting F & P

--- src/views/CurriculumView.vue
+++ src/views/CurriculumView.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <!-- <router-link to="/faculty"><button> Create Faculty</button></router-link> -->
+    <router-link to="/faculty"><button> Create Faculty</button></router-link>
     <router-link to="/courseprogram"><button> Create Program</button></router-link>
   </div>
 
-  <!-- <h3>Select Faculty:</h3>
+  <h3>Select Faculty:</h3>
   <select v-model="selectedFaculty">
     <option value="">-- Select Faculty --</option>
-    <option v-for="faculty in faculties" :value="faculty.facultyId" :key="faculty.facultyId">
+    <option v-for="faculty in faculties" :value="faculty.name" :key="faculty.facultyId">
       {{ faculty.name }}
     </option>
-  </select> -->
+  </select>
 
-  <!-- <h3 v-if="selectedFaculty">Select Program:</h3> -->
-  <h3>Select Program:</h3>
-  <select v-model="selectedProgram">
+  <h3 v-if="selectedFaculty">Select Program:</h3>
+  <select v-if="selectedFaculty" v-model="selectedProgram">
     <option value="">-- Select Program --</option>
-    <option v-for="program in programs" :value="program.programId" :key="program.programId">
+    <option v-for="program in filteredPrograms" :value="program.programId" :key="program.programId">
       {{ program.name }}
     </option>
   </select>
@@ -298,10 +297,15 @@ export default {
   computed: {
     filteredPrograms() {
       if (this.selectedFaculty) {
-        return this.programs.filter(program => program.programId === this.selectedFaculty);
+        const filteredPrograms = this.programs.filter(program => program.faculty && program.faculty.name === this.selectedFaculty);
+        console.log("Filtered programs:", filteredPrograms);
+        console.log("sf", this.selectedFaculty);
+        return filteredPrograms;
       }
+      // If no faculty is selected or there are no matching programs, return an empty array.
       return [];
-    }, getLearnerPersonCourses() {
+    },
+    getLearnerPersonCourses() {
       return this.filteredPrograms.find(program => program.programId === this.selectedProgram)?.gerclp || [];
     }, getInnovativeCoCreatorCourses() {
       return this.filteredPrograms.find(program => program.programId === this.selectedProgram)?.gercic || [];
