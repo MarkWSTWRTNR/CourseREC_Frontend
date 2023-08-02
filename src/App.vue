a2f791fdb01ee3eae9e2b20eba027abb3291212c
markwst <Wongsathorn_W@gmail.com>
2023-08-03 03:01:12 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-
+  <div>
+    <nav v-if="userRole === ROLES.ADMIN" class="navbar">
+      <router-link to="/course_list">Course list</router-link>
+      <router-link to="/curriculum">Curriculum</router-link>
+      <router-link to="/login">login</router-link>
+    </nav>
     
-      <nav v-if="userRole === ROLES.ADMIN">
-        <router-link to="/curriculum">Curriculum</router-link>
-        <router-link to="/course_list">Course list</router-link>
-      </nav>
-
-      <nav v-if="userRole === ROLES.STUDENT">
-        <router-link to="/dashboard">Dashboard</router-link>
-        <router-link to="/curriculum">Curriculum</router-link>
-        <router-link to="/course_list">Course List</router-link>
-        <router-link to="/studentstudyplan">Student Study Plan</router-link>
-        <router-link to="/recommendcourse">Recommend Course</router-link>
-        <router-link to="/studyguide">Study Guide</router-link>
-        <router-link to="/finishedcourse">Finished Course</router-link>
-      </nav>
-      <router-view />
+    <nav v-if="userRole === ROLES.STUDENT" class="navbar">
+      <router-link to="/dashboard">Dashboard</router-link>
+      <router-link to="/curriculum">Curriculum</router-link>
+      <router-link to="/course_list">Course List</router-link>
+      <router-link to="/studentstudyplan">Student Study Plan</router-link>
+      <router-link to="/recommendcourse">Recommend Course</router-link>
+      <router-link to="/studyguide">Study Guide</router-link>
+      <router-link to="/finishedcourse">Finished Course</router-link>
+    </nav>
+    <router-view />
     <!-- Footer -->
+  </div>
 </template>
 
-
 <script>
-import { userRole, ROLES } from "../src/service/roles"
+import { userRole, ROLES } from "../src/service/roles";
 
 export default {
   data() {
@@ -30,9 +30,8 @@ export default {
       ROLES: ROLES,
     };
   },
-}
+};
 </script>
-
 
 <style>
 #app {
@@ -41,25 +40,41 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
 }
 
-nav {
+.navbar {
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 30px;
-  background-color: #010508;
-  text-align: left;
+  justify-content: flex-start;
+  padding: 15px;
+  background-color: #f1f1f1;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0);
 }
 
-nav a {
-  margin-bottom: 10px;
+.navbar a {
   font-weight: bold;
-  color: #ffffff;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  margin: auto;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.navbar a:hover {
+  background-color: #e3e3e3;
+}
+
+.navbar a.router-link-exact-active {
+  background-color: #42b983;
+  color: white;
+}
+
+.router-link-exact-active {
+  background-color: #42b983;
+  color: white;
+  border-radius: 4px;
+}
+
+.router-link-exact-active:hover {
+  background-color: #3c8c6e;
 }
 </style>
--- src/components/CourseDetailView.vue
+++ src/components/CourseDetailView.vue
@@ -1,44 +1,50 @@
 <template>
-    <div>
-      <h2>{{ course.name }}</h2>
-      <p><strong>Course ID:</strong> {{ course.courseId }}</p>
-      <p><strong>Credit:</strong> {{ course.credit }}</p>
-      <p><strong>Grading Type:</strong> {{ course.gradingtype }}</p>
-      <p><strong>Course Prerequisite:</strong> 
+  <div>
+    <h2>{{ course.name }}</h2>
+    <p><strong>Course ID:</strong> {{ course.courseId }}</p>
+    <p><strong>Credit:</strong> {{ course.credit }}</p>
+    <p><strong>Grading Type:</strong> {{ course.gradingtype }}</p>
+    <p><strong>Course Prerequisite:</strong>
       <li v-for="prerequisite in course.prereq" :key="prerequisite.courseId">
         {{ prerequisite.courseId }} - {{ prerequisite.name }}
       </li>
+    <p><strong>Programs:</strong>
+      <li v-for="program in course.groupCourses" :key="program.id">
+        {{ program.programs.name }} ({{ program.programs.programId }})
+      </li>
     </p>
-      <p><strong>Description:</strong> {{ course.description }}</p>
-    </div>
-  </template>
+    </p>
+    <p><strong>Description:</strong> {{ course.description }}</p>
+  </div>
+  <p><strong></strong></p>
+</template>
   
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'CourseDetail',
-    data() {
-      return {
-        course: {}
-      };
-    },
-    mounted() {
-      const courseId = this.$route.params.courseId;
-      this.fetchCourse(courseId);
-    },
-    methods: {
-      fetchCourse(courseId) {
-        axios.get(`http://localhost:8080/courseByCourseId/${courseId}`)
-          .then(response => {
-            this.course = response.data;
-            console.log(this.course);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CourseDetail',
+  data() {
+    return {
+      course: {}
+    };
+  },
+  mounted() {
+    const courseId = this.$route.params.courseId;
+    this.fetchCourse(courseId);
+  },
+  methods: {
+    fetchCourse(courseId) {
+      axios.get(`http://localhost:8080/courseByCourseId/${courseId}`)
+        .then(response => {
+          this.course = response.data;
+          console.log(this.course);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
-  };
-  </script>
+  }
+};
+</script>
   
