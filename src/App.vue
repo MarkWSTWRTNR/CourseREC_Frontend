095eedd8f579abef5a176ed4f02172a5d535bbed
markwst <Wongsathorn_W@gmail.com>
2023-08-03 14:49:51 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    <nav v-if="userRole === ROLES.ADMIN" class="navbar">
+  <div class="container">
+    <!-- Vertical navbar on the right -->
+    <nav v-if="userRole === ROLES.ADMIN" class="navbar navbar-admin">
       <router-link to="/course_list">Course list</router-link>
       <router-link to="/curriculum">Curriculum</router-link>
       <router-link to="/login">login</router-link>
     </nav>
-    
-    <nav v-if="userRole === ROLES.STUDENT" class="navbar">
+
+    <nav v-if="userRole === ROLES.STUDENT" class="navbar navbar-student">
       <router-link to="/dashboard">Dashboard</router-link>
       <router-link to="/curriculum">Curriculum</router-link>
       <router-link to="/course_list">Course List</router-link>
@@ -15,8 +16,11 @@
       <router-link to="/studyguide">Study Guide</router-link>
       <router-link to="/finishedcourse">Finished Course</router-link>
     </nav>
-    <router-view />
-    <!-- Footer -->
+
+    <!-- Content on the left -->
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -42,12 +46,27 @@ export default {
   color: #2c3e50;
 }
 
+.container {
+  display: flex;
+}
+
+/* Update styles for the vertical navbar on the right */
 .navbar {
   display: flex;
-  justify-content: flex-start;
+  flex-direction: column; /* Display the links vertically */
   padding: 15px;
   background-color: #f1f1f1;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0);
+  width: 200px; /* Set a fixed width for the vertical navbar */
+}
+
+/* Right align the vertical navbar */
+.navbar.navbar-admin {
+  justify-content: flex-start;
+}
+
+.navbar.navbar-student {
+  justify-content: flex-start;
 }
 
 .navbar a {
@@ -56,7 +75,7 @@ export default {
   text-decoration: none;
   padding: 8px 12px;
   border-radius: 4px;
-  margin: auto;
+  margin: 4px 0;
 }
 
 .navbar a:hover {
@@ -76,5 +95,11 @@ export default {
 
 .router-link-exact-active:hover {
   background-color: #3c8c6e;
+}
+
+/* Styles for the content on the left */
+.content {
+  flex: 1; /* Fill the remaining width of the container */
+  padding: 15px;
 }
 </style>
