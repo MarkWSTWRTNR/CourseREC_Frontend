62a343c9b2a224c10c844b65ee5669bb875bf152
markwst <Wongsathorn_W@gmail.com>
2023-08-20 12:01:20 +0700

    login can work but refresh page it's error 500

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div v-if="loading">
+            Loading user info...
+        </div>
+        <div v-if="userInfo">
+            <h2>User Info</h2>
+            <pre>{{ userInfo }}</pre>
+        </div>
+    </div>
+</template>
+  
+<script>
+import LoginService from '@/service/LoginService'
+
+export default {
+    name: 'UserInfo',
+    data() {
+        return {
+            loading: true,
+            userInfo: null
+        }
+    },
+    methods: {
+        async handleRedirectWithCode(code) {
+            try {
+                // Exchange the authorization code for an access token
+                const response = await LoginService.getAccessToken({ code: code });
+                const accessToken = response.data.access_token
+                localStorage.setItem('access_token', accessToken)
+
+                // Fetch user info using the access token
+                this.fetchUserInfo(accessToken)
+            } catch (error) {
+                console.error("Error fetching access token or user info:", error)
+            }
+        },
+        async fetchUserInfo(accessToken) {
+            try {
+                const response = await LoginService.fetchUserInfo(accessToken)
+                this.userInfo = response.data
+                this.loading = false
+            } catch (error) {
+                console.error("Error fetching user info:", error)
+                this.loading = false
+            }
+        }
+    },
+    mounted() {
+        // If the user is redirected back to this component with a code in the URL, handle it
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
+        console.log("Authorization Code:", code);
+
+        if (code) {
+            this.handleRedirectWithCode(code)
+        } else {
+            this.loading = false
+        }
+    }
+}
+</script>
+  
---
+++ src/service/LoginService.js
@@ -0,0 +1,24 @@
+import apiClient from '@/service/AxiosClient.js'
+
+export default {
+  getAccessToken(formData) {
+    return apiClient.post('http://localhost:8080/callback', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+  },
+  getStoredAccessToken() {
+    return localStorage.getItem('access_token')
+  },
+  fetchUserInfo(accessToken) {
+    return apiClient.get('http://localhost:8080/userinfo', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+  },
+  login() {
+    return apiClient.get('http://localhost:8080/login')
+  }
+}
