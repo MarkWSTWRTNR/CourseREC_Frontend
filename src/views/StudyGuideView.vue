f7c3dff89edcb3b3af864c756f0674cce8753360
markwst <Wongsathorn_W@gmail.com>
2023-08-22 09:49:44 +0700

    update chat gpt

--- src/views/StudyGuideView.vue
+++ src/views/StudyGuideView.vue
@@ -1,54 +1,70 @@
-<!-- StudyGuide.vue -->
 <template>
-    <div class="container">
-    <div>
-      <h2>Study Guide Recommendations by ChatGPT</h2>
-      <button @click="getRecommendation">Get Study Guide Recommendation</button>
-      
-      <!-- Loading State -->
-      <div v-if="loading">Loading...</div>
-      
-      <!-- Display Recommendation -->
-      <p v-if="recommendation && !loading">{{ recommendation }}</p>
-    </div></div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        recommendation: '',
-        loading: false
-      };
-    },
-    methods: {
-      async getRecommendation() {
-        // Clear old recommendation
-        this.recommendation = '';
-        localStorage.removeItem('studyGuideRecommendation');
-  
-        this.loading = true; // Set loading state to true
-        try {
-          const response = await axios.get('http://localhost:8080/bot/chat');
-          this.recommendation = response.data;
-          // Save the new recommendation to localStorage
-          localStorage.setItem('studyGuideRecommendation', this.recommendation);
-        } catch (error) {
-          console.error('Error fetching recommendation:', error);
-        } finally {
-          this.loading = false; // Set loading state back to false
-        }
-      }
-    },
-    mounted() {
-      // Load recommendation from localStorage when the component is mounted
-      const storedRecommendation = localStorage.getItem('studyGuideRecommendation');
-      if (storedRecommendation) {
-        this.recommendation = storedRecommendation;
+  <div class="container">
+    <h2>Study Guide Recommendations by ChatGPT</h2>
+    <button @click="getRecommendation">Get Study Guide Recommendation</button>
+
+    <!-- Loading State -->
+    <div v-if="loading">Loading...</div>
+
+    <!-- Display Finished Courses -->
+    <div v-if="finishedCourses && !loading">
+      <h3>Finished Courses:</h3>
+      <ul>
+        <li v-for="course in finishedCourses" :key="course">{{ course }}</li>
+      </ul>
+    </div>
+
+    <!-- Display Recommendation -->
+    <div v-if="recommendation && !loading">
+      <h3>Recommendation:</h3>
+      <p>{{ recommendation }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      recommendation: '',
+      finishedCourses: [],
+      loading: false
+    };
+  },
+  methods: {
+    async getRecommendation() {
+      this.recommendation = '';
+      this.finishedCourses = [];
+      localStorage.removeItem('studyGuideData');
+
+      this.loading = true;
+      try {
+        const response = await axios.get('http://localhost:8080/bot/chat');
+        this.recommendation = response.data.recommendation;
+        this.finishedCourses = response.data.finishedCourses;
+
+        // Save the data to localStorage
+        localStorage.setItem('studyGuideData', JSON.stringify({
+          recommendation: this.recommendation,
+          finishedCourses: this.finishedCourses
+        }));
+      } catch (error) {
+        console.error('Error fetching recommendation:', error);
+      } finally {
+        this.loading = false;
       }
     }
-  };
-  </script>
-  
+  },
+  mounted() {
+    // Load data from localStorage when the component is mounted
+    const storedData = localStorage.getItem('studyGuideData');
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      this.recommendation = parsedData.recommendation;
+      this.finishedCourses = parsedData.finishedCourses;
+    }
+  }
+};
+</script>
